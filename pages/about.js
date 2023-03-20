d97d8dcea274571fe0a02f5b24b2c880d7d0a17d
AzureAD\HazemTwair <htwair@itgsoftware.com>
2023-03-20 22:12:43 +0200

    initial

--- pages/about.js
+++ pages/about.js
@@ -1,28 +1,25 @@
-import styles from "@/styles/Home.module.css";
 import Head from "next/head";
-import Link from "next/link";
 
-export default function Home() {
+const About = () => {
   return (
     <>
       <Head>
-        <title>Ninja List | Home</title>
+        <title>Ninja List | About</title>
         <meta name="keywords" content="ninjas" />
       </Head>
       <div>
-        <h1 className={styles.title}>Homepage</h1>
-        <p className={styles.text}>
+        <h1>About</h1>
+        <p>
           lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem
           ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum{" "}
         </p>
-        <p className={styles.text}>
+        <p>
           lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem
           ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum{" "}
         </p>
-        <Link href="/ninjas" className={styles.btn}>
-          See Ninja Listing
-        </Link>
       </div>
     </>
   );
-}
+};
+
+export default About;
